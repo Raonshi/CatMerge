3da3e25f195a2e953babb9f8aa4db4eddfd70275
sunwonhong <sunwonsw9561@gmail.com>
2020-11-19 13:06:27 +0900

    commmit to Revert

--- 2048/Assets/Scripts/UI/OptionPanel.cs
+++ 2048/Assets/Scripts/UI/OptionPanel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class OptionPanel : MonoBehaviour
 {
@@ -16,6 +17,11 @@ public class OptionPanel : MonoBehaviour
 
     private void Start()
     {
+        if(SceneManager.GetActiveScene().name == "Game")
+        {
+            Game.instance.isClose = false;
+        }
+
         bgmSlider = bgm.GetComponentInChildren<Slider>();
         sfxSlider = sfx.GetComponentInChildren<Slider>();
 
@@ -36,6 +42,11 @@ public class OptionPanel : MonoBehaviour
 
     public void OnClickClose()
     {
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            Game.instance.isClose = true;
+        }
+
         SoundManager.Singleton.PlaySound(Resources.Load<AudioClip>("Sounds/SFX_Click"));
         gameObject.SetActive(false);
     }
