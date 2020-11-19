022314e1905c85f9df8c2e29171eb4b029d04e10
sunwonhong <sunwonsw9561@gmail.com>
2020-11-19 15:47:00 +0900

    20201119 -commit 구글 애드몹 테스트 페이지 출력 성공

---
+++ 2048/Assets/Scripts/Ads.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class Ads : MonoBehaviour
+{
+    private BannerView bannerView;
+
+    // Use this for initialization
+    void Start()
+    {
+        RequestBanner();
+    }
+
+    /*
+    public void OnGUI()
+    {
+        GUI.skin.label.fontSize = 60;
+        Rect textOutputRect = new Rect(
+          0.15f * Screen.width,
+          0.25f * Screen.height,
+          0.7f * Screen.width,
+          0.3f * Screen.height);
+        GUI.Label(textOutputRect, "Adaptive Banner Example");
+    }
+    */
+
+    private void RequestBanner()
+    {
+        // These ad units are configured to always serve test ads.
+#if UNITY_EDITOR
+        string adUnitId = "unused";
+#elif UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3212738706492790/5381898163";
+#else
+            string adUnitId = "unexpected_platform";
+#endif
+
+        // Clean up banner ad before creating a new one.
+        if (this.bannerView != null)
+        {
+            this.bannerView.Destroy();
+        }
+
+        AdSize adaptiveSize =
+                AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
+
+        this.bannerView = new BannerView(adUnitId, adaptiveSize, AdPosition.Bottom);
+
+        // Register for ad events.
+        this.bannerView.OnAdLoaded += this.HandleAdLoaded;
+        this.bannerView.OnAdFailedToLoad += this.HandleAdFailedToLoad;
+        this.bannerView.OnAdOpening += this.HandleAdOpened;
+        this.bannerView.OnAdClosed += this.HandleAdClosed;
+        this.bannerView.OnAdLeavingApplication += this.HandleAdLeftApplication;
+
+        AdRequest adRequest = new AdRequest.Builder()
+            .AddTestDevice(AdRequest.TestDeviceSimulator)
+            .AddTestDevice("0123456789ABCDEF0123456789ABCDEF")
+            .Build();
+
+        // Load a banner ad.
+        this.bannerView.LoadAd(adRequest);
+    }
+
+    #region Banner callback handlers
+
+    public void HandleAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+        MonoBehaviour.print(String.Format("Ad Height: {0}, width: {1}",
+            this.bannerView.GetHeightInPixels(),
+            this.bannerView.GetWidthInPixels()));
+    }
+
+    public void HandleAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print(
+                "HandleFailedToReceiveAd event received with message: " + args.Message);
+    }
+
+    public void HandleAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleAdLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeftApplication event received");
+    }
+
+    #endregion
+}
