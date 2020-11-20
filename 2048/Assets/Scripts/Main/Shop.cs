672431df03d42996c6d5cf72791089e172100090
sunwonhong <sunwonsw9561@gmail.com>
2020-11-20 17:54:15 +0900

    20201120 commit Shop

---
+++ 2048/Assets/Scripts/Main/Shop.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnClickBuyCat(int id)
+    {
+        switch(id)
+        {
+            case 1:
+
+
+                break;
+
+            case 2:
+
+
+                break;
+
+            case 3:
+
+
+                break;
+
+            case 4:
+
+
+                break;
+
+            case 5
+            
+            
+            :
+
+
+                break;
+        }
+    }
+}
